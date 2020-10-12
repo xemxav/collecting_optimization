a2bf2ac244e9c1b34856faf93368051ca61c90fd
xemxav <xavier.emmanuel.moreau@gmail.com>
2020-10-12 10:51:44 +0200

    documentation for reference.py

--- reference.py
+++ reference.py
@@ -1,18 +1,22 @@
-ID = "ID"
-INLET = "Inlet"
-ILA = "Inlet_Latitude"
-ILO = "Inlet_Longitude"
-MONTH = "Month"
-DATE = "Execution_date"
-DRIPLATE = "Immatriculation"
-DRIVER = "Driver"
-CNAME = "Client_name"
-CADR = "Client_adress"
-CLA = "Client_Latitude"
-CLO = "Client_Longitude"
-MATERIAL = "Transport_type"
-OUTLET = "Outlet"
-OLO = "Outlet_Longitude"
-OLA = "Outlet_latitude"
+"""
+This file gives the possibility to customize the headers'name for the excel sheet to use.
+You must submit a valid API key from openrouteservice.org, do not upload your key in a public repository
+"""
 
-APIKEY = "5b3ce3597851110001cf6248435db7a68fe148e1888617695f66133c"
+ID = "ID"                       # Unique ID for each tour
+INLET = "Inlet"                 # Name of the starting point
+ILA = "Inlet_Latitude"          # Longitude coordinate of starting point
+ILO = "Inlet_Longitude"         # Latitude coordinate of starting point
+DATE = "Execution_date"         # Date of execution
+DRIPLATE = "Immatriculation"    # Truck's plate number
+DRIVER = "Driver"               # Truck Driver's name
+CNAME = "Client_name"           # Client's name (ie a point of collect)
+CADR = "Client_adress"          # Client's adress
+CLA = "Client_Latitude"         # Client's Longitude coordinate
+CLO = "Client_Longitude"        # Client's Latitude coordinate
+MATERIAL = "Transport_type"     # Type of marchandise collected
+OUTLET = "Outlet"               # The name of the end point
+OLO = "Outlet_Longitude"        # End point's Longitude coordinate
+OLA = "Outlet_latitude"         # End point's Longitude coordinate
+
+APIKEY = "5b3ce3597851110001cf6248435db7a68fe148e1888617695f66133c"     # insert your API key between the quotes
